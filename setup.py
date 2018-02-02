33205d25d751af9f075e1c16d82f2f61e562fecc
SenbinYu <justdoit920823@gmail.com>
2018-02-02 23:01:19 +0800

    Release version 0.1.2 .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = '0.1.1'
+version = '0.1.2'
 
 
 class PyTest(TestCommand):
